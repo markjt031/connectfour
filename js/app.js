1b4365489d185f9e8fc258a6fa0b57eea797fd02
markjt031 <markjt031@gmail.com>
2023-01-28 13:39:30 -0500

    Buttons on the top of the grid will place pieces when clicked

--- js/app.js
+++ js/app.js
@@ -20,13 +20,22 @@ class Game{
             this.grid.push(row);
         }
     }
+    addButtonListeners(playerColor){
+        let buttons=document.getElementsByClassName("place-piece-button");
+        for (let i=0; i<this.columns;i++){
+            let columnNumber=buttons[i].id.toString();
+            buttons[i].addEventListener("click", ()=>this.placePiece(playerColor,columnNumber))
+        }
+    }
     playGame(){
         this.populateGrid();
-        this.placePiece("red", 0);
-        this.placePiece("yellow", 0);
-        this.placePiece("red", 6);
+        //this.placePiece("red", 0);
+        //this.placePiece("yellow", 0);
+       // this.placePiece("red", 6);
+       this.addButtonListeners("red");
     }
     placePiece(color, columnNumber){
+        console.log("I'm running")
         for (let i=this.rows-1; i>=0; i--){
             if (this.grid[i][columnNumber].isEmpty===true){
                 this.grid[i][columnNumber].color=color;
