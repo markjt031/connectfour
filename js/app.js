511892e19f64e0f572ea45dabc7b190863e58aee
markjt031 <markjt031@gmail.com>
2023-01-28 13:13:18 -0500

    Basic function to add pieces manually is functional(for now)

--- js/app.js
+++ js/app.js
@@ -10,28 +10,44 @@ class Game{
         for (let i=0; i<this.rows; i++){
             let row=[]
             for(let j=0; j<this.columns; j++){
-                let htmlTile=document.createElement("div")
-                htmlTile.classList.add("tile")
-                let tile=new GridTile(i, j, htmlTile)
-                row.push(tile)
+                let htmlTile=document.createElement("div");
+                htmlTile.classList.add("tile");
+                let tile=new GridTile(i, j, htmlTile);
+                row.push(tile);
                 this.board.appendChild(tile.htmlTile);
-                console.log(htmlTile)
+                console.log(htmlTile);
             }
-            this.grid.push(row)
+            this.grid.push(row);
+        }
+    }
+    playGame(){
+        this.populateGrid();
+        this.placePiece("red", 0);
+        this.placePiece("yellow", 0);
+    }
+    placePiece(color, columnNumber){
+        for (let i=this.rows-1; i>=0; i--){
+            if (this.grid[i][columnNumber].isEmpty===true){
+                this.grid[i][columnNumber].color=color;
+                this.grid[i][columnNumber].isEmpty=false;
+                this.grid[i][columnNumber].htmlTile.style.backgroundColor=color;
+                break;
+            }
         }
     }
 }
 class GridTile{
     constructor(rowPosition, columnPosition, htmlTile){
         this.isEmpty=true;
-        this.color=""
-        this.rowPosition=rowPosition
-        this.columnPosition=columnPosition
+        this.color="";
+        this.rowPosition=rowPosition;
+        this.columnPosition=columnPosition;
         this.htmlTile=htmlTile;
     }
 }
 
 const game=new Game;
-game.populateGrid();
+//game.populateGrid();
 //console.log(game.grid)
+game.playGame();
 console.log(game.board)
